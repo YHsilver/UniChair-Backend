0697a13527adb1352219a14a1a3e5b0680d8f55e
Pan xingyu <48406665+icecream154@users.noreply.github.com>
2020-05-05 12:33:12 +0800

    admin service test

--- src/test/java/fudan/se/lab2/service/adminPage/AdminServiceTest.java
+++ src/test/java/fudan/se/lab2/service/adminPage/AdminServiceTest.java
@@ -4,6 +4,8 @@ import fudan.se.lab2.controller.adminPage.request.AdminChangeConferenceStatusReq
 import fudan.se.lab2.controller.adminPage.request.AdminGetConferenceApplicationsRequest;
 import fudan.se.lab2.domain.User;
 import fudan.se.lab2.domain.conference.Conference;
+import fudan.se.lab2.generator.ConferenceGenerator;
+import fudan.se.lab2.generator.UserGenerator;
 import fudan.se.lab2.repository.ConferenceRepository;
 import fudan.se.lab2.repository.TopicRepository;
 import fudan.se.lab2.repository.UserRepository;
@@ -24,98 +26,72 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class AdminServiceTest {
     private ConferenceRepository conferenceRepository;
-    private TopicRepository topicRepository;
+    private UserRepository userRepository;
     private AdminService adminService;
-    private Conference testConference1;
-    private Conference testConference2;
-    private Conference testConference3;
 
 
     // constructor
     @Autowired
     public AdminServiceTest(ConferenceRepository conferenceRepository, TopicRepository topicRepository, UserRepository userRepository) {
         this.conferenceRepository = conferenceRepository;
-        this.topicRepository = topicRepository;
+        this.userRepository = userRepository;
         adminService = new AdminService(conferenceRepository, topicRepository);
-
-        User UserTest1 = new User(
-                "UserTest1",
-                "UserTest1pass",
-                "UserTest1fullName",
-                "Test",
-                "test",
-                "test@fudan.edu.cn"
-        );
-
-        //todo  bug
-        userRepository.save(UserTest1);
-
-
-        String[] topicSet = {"testTopic1", "testTopic2"};
-        testConference1 = new Conference(userRepository.findByUsername("UserTest1"),
-                "UserTest1", "AI full name", "AI location",
-                LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12),
-                LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12),
-                "AI introduction", topicSet);
-        testConference2 = new Conference(userRepository.findByUsername("UserTest1"),
-                "UserTest1", "AI full name", "AI location",
-                LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12),
-                LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12),
-                "AI introduction", topicSet);
-        testConference3 = new Conference(userRepository.findByUsername("UserTest1"),
-                "UserTest1", "AI full name", "AI location",
-                LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12),
-                LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12),
-                "AI introduction", topicSet);
-        testConference1.setStatus(Conference.Status.PASS);
-        testConference1.setConferenceId(1L);
-        testConference2.setStatus(Conference.Status.PENDING);
-        testConference2.setConferenceId(2L);
-        testConference3.setStatus(Conference.Status.REJECT);
-        testConference3.setConferenceId(3L);
-        this.conferenceRepository.save(testConference1);
-        this.conferenceRepository.save(testConference2);
-        this.conferenceRepository.save(testConference3);
-
-        System.out.println( conferenceRepository.findByConferenceId(1L).getChairman());
     }
 
 
     @Test
     void getConferenceApplications() {
+        User user = UserGenerator.getRandomUser();
+        userRepository.save(user);
+        Conference conference1 = ConferenceGenerator.getRandomConference(user);
+        Conference conference2 = ConferenceGenerator.getRandomConference(user);
+        Conference conference3 = ConferenceGenerator.getRandomConference(user);
+        conference1.setStatus(Conference.Status.PENDING);
+        conference2.setStatus(Conference.Status.PASS);
+        conference3.setStatus(Conference.Status.REJECT);
+        conferenceRepository.save(conference1);
+        conferenceRepository.save(conference2);
+        conferenceRepository.save(conference3);
 
+        AdminGetConferenceApplicationsRequest null_request = new AdminGetConferenceApplicationsRequest(null);
         AdminGetConferenceApplicationsRequest pending_request = new AdminGetConferenceApplicationsRequest(Conference.Status.PENDING);
         AdminGetConferenceApplicationsRequest pass_request = new AdminGetConferenceApplicationsRequest(Conference.Status.PASS);
         AdminGetConferenceApplicationsRequest reject_request = new AdminGetConferenceApplicationsRequest(Conference.Status.REJECT);
         List<JSONObject> list;
 
-        list = adminService.getConferenceApplications(pass_request);
-        assertEquals(testConference1.toBriefJson(), list.get(0));
+        list = adminService.getConferenceApplications(null_request);
+        assertEquals(0, list.size());
         list = adminService.getConferenceApplications(pending_request);
-        assertEquals(testConference2.toBriefJson(), list.get(0));
+        assertEquals(conference1.toBriefJson(), list.get(0));
+        list = adminService.getConferenceApplications(pass_request);
+        assertEquals(conference2.toBriefJson(), list.get(0));
         list = adminService.getConferenceApplications(reject_request);
-        assertEquals(testConference3.toBriefJson(), list.get(0));
+        assertEquals(conference3.toBriefJson(), list.get(0));
     }
 
     @Test
     void changeConferenceStatus() {
+        User user = UserGenerator.getRandomUser();
+        userRepository.save(user);
+        Conference conference = ConferenceGenerator.getRandomConference(user);
+        conferenceRepository.save(conference);
 
-        AdminChangeConferenceStatusRequest adminChangeConferenceStatusRequest = new AdminChangeConferenceStatusRequest(1L, Conference.Status.REJECT);
+        AdminChangeConferenceStatusRequest adminChangeConferenceStatusNullRequest =
+                new AdminChangeConferenceStatusRequest(conference.getConferenceId(), null);
+        assertEquals("Invalid Request", adminService.changeConferenceStatus(adminChangeConferenceStatusNullRequest));
+
+        AdminChangeConferenceStatusRequest adminChangeConferenceStatusRequest =
+                new AdminChangeConferenceStatusRequest(conference.getConferenceId(), Conference.Status.REJECT);
         adminService.changeConferenceStatus(adminChangeConferenceStatusRequest);
-        Conference conference = conferenceRepository.findByConferenceId(1L);
-        assertEquals(Conference.Status.REJECT, conference.getStatus());
+        assertEquals(Conference.Status.REJECT, conferenceRepository.findByConferenceId(conference.getConferenceId()).getStatus());
 
-
-        //todo bug
-        AdminChangeConferenceStatusRequest adminChangeConferenceStatusRequest2 = new AdminChangeConferenceStatusRequest(1L, Conference.Status.PASS);
+        AdminChangeConferenceStatusRequest adminChangeConferenceStatusRequest2 = new AdminChangeConferenceStatusRequest(conference.getConferenceId(), Conference.Status.PASS);
         adminService.changeConferenceStatus(adminChangeConferenceStatusRequest2);
-        Conference conference2 = conferenceRepository.findByConferenceId(1L);
-        assertEquals(Conference.Status.PASS, conference2.getStatus());
+        assertEquals(Conference.Status.PASS, conferenceRepository.findByConferenceId(conference.getConferenceId()).getStatus());
 
-        AdminChangeConferenceStatusRequest adminChangeConferenceStatusRequest3 = new AdminChangeConferenceStatusRequest(1L, Conference.Status.PENDING);
+        AdminChangeConferenceStatusRequest adminChangeConferenceStatusRequest3 = new AdminChangeConferenceStatusRequest(conference.getConferenceId(), Conference.Status.PENDING);
         adminService.changeConferenceStatus(adminChangeConferenceStatusRequest3);
-        Conference conference3 = conferenceRepository.findByConferenceId(1L);
-        assertEquals(Conference.Status.PENDING, conference3.getStatus());
+        assertEquals(Conference.Status.PENDING, conferenceRepository.findByConferenceId(conference.getConferenceId()).getStatus());
 
 
     }
