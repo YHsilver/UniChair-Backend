1b21559bdd497db195fc094baf905eb3a3ef2e18
Hu yufeng <18307130207@fudan.edu.cn>
2020-05-06 16:56:20 +0800

    fix bugs

--- src/main/java/fudan/se/lab2/domain/conference/Conference.java
+++ src/main/java/fudan/se/lab2/domain/conference/Conference.java
@@ -3,15 +3,12 @@ package fudan.se.lab2.domain.conference;
 import fudan.se.lab2.domain.User;
 import fudan.se.lab2.service.UtilityService;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -99,11 +96,11 @@ public class Conference implements Serializable {
         this.stage = Stage.PREPARATION;// 初始化都是准备状态
 
         //this.conferenceAbstract = new ConferenceAbstract(this.conferenceId, conferenceAbbreviation, conferenceFullName,
-                //conferenceLocation, conferenceTime, contributeStartTime, contributeEndTime, resultReleaseTime,
-                //introduction, this.status, this.stage);
+        //conferenceLocation, conferenceTime, contributeStartTime, contributeEndTime, resultReleaseTime,
+        //introduction, this.status, this.stage);
     }
 
-    public void addAuthor(User author){
+    public void addAuthor(User author) {
         this.authorSet.add(author);
     }
 
@@ -147,7 +144,9 @@ public class Conference implements Serializable {
         return introduction;
     }
 
-    public String[] getTopics() { return topics; }
+    public String[] getTopics() {
+        return topics;
+    }
 
     public User getChairman() {
         return chairman;
@@ -205,7 +204,9 @@ public class Conference implements Serializable {
         this.introduction = introduction;
     }
 
-    public void setTopics(String[] topics) { this.topics = topics; }
+    public void setTopics(String[] topics) {
+        this.topics = topics;
+    }
 
     public void setContributeEndTime(LocalDate contributeEndTime) {
         this.contributeEndTime = contributeEndTime;
@@ -247,9 +248,9 @@ public class Conference implements Serializable {
         this.reviewerAndPapersMap = reviewerAndPapersMap;
     }
 
-    public Set<Paper> getPapersOfReviewer(User reviewer){
+    public Set<Paper> getPapersOfReviewer(User reviewer) {
         for (Review review : reviewerAndPapersMap) {
-            if(review.getReviewer().getId().equals(reviewer.getId())){
+            if (review.getReviewer().getId().equals(reviewer.getId())) {
                 return review.getPapers();
             }
         }
@@ -278,25 +279,25 @@ public class Conference implements Serializable {
                 '}';
     }
 
-    public boolean isNextStage(Stage tarStage){
-        switch (stage){
+    public boolean isNextStage(Stage tarStage) {
+        switch (stage) {
             case PREPARATION:
                 return tarStage == Stage.CONTRIBUTION;
             case CONTRIBUTION:
-                return  tarStage == Stage.REVIEWING;
+                return tarStage == Stage.REVIEWING;
             case REVIEWING:
-                return  tarStage == Stage.GRADING;
+                return tarStage == Stage.GRADING;
             case GRADING:
-                return  tarStage == Stage.ENDING;
+                return tarStage == Stage.ENDING;
             case ENDING:
                 return false;
         }
         return false;
     }
 
-    public boolean isAfterOrEqualsStage(Stage tarStage){
-        while(tarStage != null){
-            if(tarStage == stage){
+    public boolean isAfterOrEqualsStage(Stage tarStage) {
+        while (tarStage != null) {
+            if (tarStage == stage) {
                 return true;
             }
             tarStage = UtilityService.getNextStage(tarStage);
@@ -304,15 +305,14 @@ public class Conference implements Serializable {
         return false;
     }
 
-    public Topic findTopic(String topicName){
-        for(Topic topicInSet: topicSet){
-            if(topicInSet.getTopic().equals(topicName)){
+    public Topic findTopic(String topicName) {
+        for (Topic topicInSet : topicSet) {
+            if (topicInSet.getTopic().equals(topicName)) {
                 return topicInSet;
             }
         }
         return null;
     }
-
 
 
     public JSONObject toBriefJson() {
@@ -353,7 +353,6 @@ public class Conference implements Serializable {
             authorFullNames[i++] = author.getFullName();
         }
         String authors = UtilityService.getJsonStringFromArray(authorFullNames);
-
         try {
             String str = "{" +
                     "\"id\":\"" + conferenceId.toString() + '\"' +
