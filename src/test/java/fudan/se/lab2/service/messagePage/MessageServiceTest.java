87b98fe669c41d479ff2b1b0e29b6343afcb03aa
Hu yufeng <18307130207@fudan.edu.cn>
2020-05-06 17:43:21 +0800

    fix bugs

--- src/test/java/fudan/se/lab2/service/messagePage/MessageServiceTest.java
+++ src/test/java/fudan/se/lab2/service/messagePage/MessageServiceTest.java
@@ -24,31 +24,29 @@ class MessageServiceTest {
     private ConferenceRepository conferenceRepository;
     private InvitationRepository invitationRepository;
     private ReviewRepository reviewRepository;
-    private TopicRepository topicRepository;
     private JwtTokenUtil tokenUtil;
     private MessageService messageService;
 
     @Autowired
-    public MessageServiceTest(UserRepository userRepository, InvitationRepository invitationRepository, TopicRepository topicRepository,
-                          ConferenceRepository conferenceRepository, ReviewRepository reviewRepository, JwtTokenUtil tokenUtil) {
+    public MessageServiceTest(UserRepository userRepository, InvitationRepository invitationRepository,
+                              ConferenceRepository conferenceRepository, ReviewRepository reviewRepository, JwtTokenUtil tokenUtil) {
         this.userRepository = userRepository;
         this.conferenceRepository = conferenceRepository;
-        this.topicRepository = topicRepository;
         this.tokenUtil = tokenUtil;
         this.invitationRepository = invitationRepository;
         this.reviewRepository = reviewRepository;
-        messageService=new MessageService(userRepository,invitationRepository,conferenceRepository,reviewRepository,tokenUtil);
+        messageService = new MessageService(userRepository, invitationRepository, conferenceRepository, reviewRepository, tokenUtil);
     }
 
 
     @Test
     void userCheckMyInvitations() {
-        User chair= UserGenerator.getRandomUser();
-        User user1=UserGenerator.getRandomUser();
+        User chair = UserGenerator.getRandomUser();
+        User user1 = UserGenerator.getRandomUser();
         userRepository.save(chair);
         userRepository.save(user1);
 
-        Conference conference= ConferenceGenerator.getRandomConference(chair);
+        Conference conference = ConferenceGenerator.getRandomConference(chair);
         conference.setStatus(Conference.Status.PASS);
         conferenceRepository.save(conference);
 
@@ -60,34 +58,24 @@ class MessageServiceTest {
         userRepository.save(user1);
         userRepository.save(chair);
 
-        UserCheckMyInvitationsRequest userCheckMyInvitationsRequest=new UserCheckMyInvitationsRequest(
-                tokenUtil.generateToken(user1),Invitation.Status.PENDING
+        UserCheckMyInvitationsRequest userCheckMyInvitationsRequest = new UserCheckMyInvitationsRequest(
+                tokenUtil.generateToken(user1), Invitation.Status.PENDING
         );
-       assertEquals(newInvitation.toStandardJson(),
-               messageService.userCheckMyInvitations(userCheckMyInvitationsRequest).get(0));
-
-
-
+        assertEquals(newInvitation.toStandardJson(),
+                messageService.userCheckMyInvitations(userCheckMyInvitationsRequest).get(0));
 
 
     }
 
     @Test
     void userDecideInvitations() {
-        User chair= UserGenerator.getRandomUser();
-        User user1=UserGenerator.getRandomUser();
+        User chair = UserGenerator.getRandomUser();
+        User user1 = UserGenerator.getRandomUser();
         userRepository.save(chair);
         userRepository.save(user1);
 
-        Conference conference= ConferenceGenerator.getRandomConference(chair);
+        Conference conference = ConferenceGenerator.getRandomConference(chair);
         conference.setStatus(Conference.Status.PASS);
-        conferenceRepository.save(conference);
-        for (String topic: conference.getTopics()
-             ) {
-            Topic tmpTopic = new Topic(conference, topic);
-            conference.getTopicSet().add(tmpTopic);
-            topicRepository.save(tmpTopic);
-        }
         conferenceRepository.save(conference);
 
         Invitation newInvitation = new Invitation(conference.getConferenceId(), conference.getConferenceFullName(), chair,
@@ -98,20 +86,18 @@ class MessageServiceTest {
         userRepository.save(user1);
         userRepository.save(chair);
 
+        String[] topics = {conference.getTopics()[0]};
 
-        String [] topics={conference.getTopics()[0]};
-
-        UserDecideInvitationsRequest userDecideInvitationsRequest=new UserDecideInvitationsRequest(
+        UserDecideInvitationsRequest userDecideInvitationsRequest = new UserDecideInvitationsRequest(
                 tokenUtil.generateToken(user1),
                 newInvitation.getInvitationId(),
                 Invitation.Status.PASS,
-                 topics
+                topics
         );
         messageService.userDecideInvitations(userDecideInvitationsRequest);
 
-        assertEquals(Invitation.Status.PASS, invitationRepository.findByInvitationId(newInvitation.getInvitationId()).getStatus());
-        assertEquals(1, conferenceRepository.findByConferenceId(conference.getConferenceId()).getReviewerSet().size());
-
+        assertEquals(Invitation.Status.PASS, newInvitation.getStatus());
+        assertEquals(1, conference.getReviewerSet().size());
 
 
     }
