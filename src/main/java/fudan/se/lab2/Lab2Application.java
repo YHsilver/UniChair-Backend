686d09040686f4e31b779f5d79a76f1a6592a2e8
Hu yufeng <18307130207@fudan.edu.cn>
2020-05-10 00:03:30 +0800

    fix bugs

--- src/main/java/fudan/se/lab2/Lab2Application.java
+++ src/main/java/fudan/se/lab2/Lab2Application.java
@@ -1,25 +1,11 @@
 package fudan.se.lab2;
 
-import fudan.se.lab2.controller.conferencePage.conferenceDetailsPage.request.chairIndentity.ChairSendInvitationRequest;
-import fudan.se.lab2.controller.messagePage.request.UserDecideInvitationsRequest;
 import fudan.se.lab2.domain.User;
-import fudan.se.lab2.domain.conference.Conference;
-import fudan.se.lab2.domain.conference.Invitation;
-import fudan.se.lab2.generator.ConferenceGenerator;
-import fudan.se.lab2.generator.UserGenerator;
-import fudan.se.lab2.repository.*;
-import fudan.se.lab2.security.jwt.JwtTokenUtil;
-import fudan.se.lab2.service.conferencePage.conferenceDetailsPage.ChairIdentityService;
-import fudan.se.lab2.service.messagePage.MessageService;
+import fudan.se.lab2.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Set;
 
 /**
  * Welcome to 2020 Software Engineering Lab2.
@@ -43,14 +29,15 @@ public class Lab2Application {
      * You can change it as you like.
      */
 
-    @Bean
-    public CommandLineRunner dataLoader(UserRepository userRepository,
-                                        ConferenceRepository conferenceRepository,
-                                        PasswordEncoder passwordEncoder,
-                                        PaperRepository paperRepository,
-                                        InvitationRepository invitationRepository,
-                                        ReviewRepository reviewRepository,
-                                        JwtTokenUtil tokenUtil) {
+//    @Bean
+//    public CommandLineRunner dataLoader(UserRepository userRepository,
+//                                        ConferenceRepository conferenceRepository,
+//                                        PasswordEncoder passwordEncoder,
+//                                        PaperRepository paperRepository,
+//                                        InvitationRepository invitationRepository,
+//                                        ReviewRepository reviewRepository,
+//                                        JwtTokenUtil tokenUtil) {
+    public CommandLineRunner dataLoader(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             // Create authorities if not exist.
             // 管理员、投稿人、审稿人
@@ -100,122 +87,122 @@ public class Lab2Application {
                 userRepository.save(AI);
             }
 
-            if (userRepository.findByUsername("robert1") == null) {
-                User AI = new User(
-                        "robert1",
-                        passwordEncoder.encode("pass"),
-                        "robert1",
-                        "fudan",
-                        "software",
-                        "AI@fudan.edu.cn"
-                );
-                userRepository.save(AI);
-            }
-            if (userRepository.findByUsername("robert2") == null) {
-                User AI = new User(
-                        "robert2",
-                        passwordEncoder.encode("pass"),
-                        "robert2",
-                        "fudan",
-                        "software",
-                        "AI@fudan.edu.cn"
-                );
-                userRepository.save(AI);
-            }
-
-            if (userRepository.findByUsername("robert3") == null) {
-                User AI = new User(
-                        "robert3",
-                        passwordEncoder.encode("pass"),
-                        "robert3",
-                        "fudan",
-                        "software",
-                        "AI@fudan.edu.cn"
-                );
-                userRepository.save(AI);
-            }
-
-            String[] topicSet = new String[]{"AI", "Robert"};
-            // add inner conferences
-            Conference AIConference = new Conference(userRepository.findByUsername("AI"), "AI abbr", "AI full name", "AI location", LocalDate.of(2020, 4, 12),
-                    LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), "AI introduction", topicSet);
-            AIConference.setTopics(new String[]{"AI TOPIC1", "AI TOPIC2"});
-            AIConference.setStatus(Conference.Status.PASS);
-            AIConference.setStage(Conference.Stage.CONTRIBUTION);
-
-            Conference testRobertConference = new Conference(userRepository.findByUsername("testRobert"), "testRobert abbr", "testRobert full name", "testRobert location", LocalDate.of(2020, 4, 12),
-                    LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), "testRobert introduction", topicSet);
-            testRobertConference.setStatus(Conference.Status.PASS);
-            testRobertConference.setTopics(new String[]{"RO TOPIC1", "RO TOPIC2"});
-            testRobertConference.setStage(Conference.Stage.CONTRIBUTION);
-
-            Conference testRobertConference2 = new Conference(userRepository.findByUsername("testRobert"), "testRobert2 abbr", "testRobert2 full name", "testRobert2 location", LocalDate.of(2020, 4, 12),
-                    LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), "testRobert2 introduction", topicSet);
-            testRobertConference2.setStatus(Conference.Status.PASS);
-            testRobertConference2.setTopics(new String[]{"RO TOPIC1", "RO TOPIC2"});
-            testRobertConference2.setStage(Conference.Stage.CONTRIBUTION);
-
-            User tempUser = userRepository.findByUsername("AI");
-            conferenceRepository.save(AIConference);
-            userRepository.save(tempUser);
-
-            tempUser = userRepository.findByUsername("testRobert");
-            conferenceRepository.save(testRobertConference);
-            userRepository.save(tempUser);
-//                userRepository.findByUsername("testRobert").addConference(testRobertConference);
-
-
-            tempUser = userRepository.findByUsername("testRobert");
-            conferenceRepository.save(testRobertConference2);
-            userRepository.save(tempUser);
-
-            Set<User> userSet = UserGenerator.getRandomUsers(20);
-            Set<Conference> conferenceSet = ConferenceGenerator.getRandomConferences(20, userSet);
-            for (User user : userSet
-            ) {
-                userRepository.save(user);
-            }
-            for (Conference conference : conferenceSet
-            ) {
-                conference.setStatus(Conference.Status.PASS);
-                conferenceRepository.save(conference);
-            }
-            // send invitations
-            ChairIdentityService chairIdentityService = chairIdentityService = new ChairIdentityService(userRepository,
-                    invitationRepository, conferenceRepository, paperRepository, reviewRepository, tokenUtil);
-
-            ChairSendInvitationRequest chairSendInvitationRequest = new ChairSendInvitationRequest(
-                    tokenUtil.generateToken(userRepository.findByUsername("AI")),
-                    AIConference.getConferenceId(),
-                    new String[]{"robert1", "robert2", "robert3"},
-                    "message"
-
-            );
-            chairIdentityService.sendInvitation(chairSendInvitationRequest);
-
-            // become PC Member
-            MessageService messageService = new MessageService(userRepository, invitationRepository, conferenceRepository, reviewRepository, tokenUtil);
-            UserDecideInvitationsRequest userDecideInvitationsRequest1 = new UserDecideInvitationsRequest(
-                    tokenUtil.generateToken(userRepository.findByUsername("robert1")),
-                    new ArrayList<>(invitationRepository.findByReviewer(userRepository.findByUsername("robert1"))).get(0).getInvitationId(),
-                    Invitation.Status.PASS,
-                    AIConference.getTopics()
-            );
-            messageService.userDecideInvitations(userDecideInvitationsRequest1);
-            UserDecideInvitationsRequest userDecideInvitationsRequest2 = new UserDecideInvitationsRequest(
-                    tokenUtil.generateToken(userRepository.findByUsername("robert2")),
-                    new ArrayList<>(invitationRepository.findByReviewer(userRepository.findByUsername("robert2"))).get(0).getInvitationId(),
-                    Invitation.Status.PASS,
-                    AIConference.getTopics()
-            );
-            messageService.userDecideInvitations(userDecideInvitationsRequest2);
-            UserDecideInvitationsRequest userDecideInvitationsRequest3 = new UserDecideInvitationsRequest(
-                    tokenUtil.generateToken(userRepository.findByUsername("robert3")),
-                    new ArrayList<>(invitationRepository.findByReviewer(userRepository.findByUsername("robert3"))).get(0).getInvitationId(),
-                    Invitation.Status.PASS,
-                    AIConference.getTopics()
-            );
-            messageService.userDecideInvitations(userDecideInvitationsRequest3);
+//            if (userRepository.findByUsername("robert1") == null) {
+//                User AI = new User(
+//                        "robert1",
+//                        passwordEncoder.encode("pass"),
+//                        "robert1",
+//                        "fudan",
+//                        "software",
+//                        "AI@fudan.edu.cn"
+//                );
+//                userRepository.save(AI);
+//            }
+//            if (userRepository.findByUsername("robert2") == null) {
+//                User AI = new User(
+//                        "robert2",
+//                        passwordEncoder.encode("pass"),
+//                        "robert2",
+//                        "fudan",
+//                        "software",
+//                        "AI@fudan.edu.cn"
+//                );
+//                userRepository.save(AI);
+//            }
+//
+//            if (userRepository.findByUsername("robert3") == null) {
+//                User AI = new User(
+//                        "robert3",
+//                        passwordEncoder.encode("pass"),
+//                        "robert3",
+//                        "fudan",
+//                        "software",
+//                        "AI@fudan.edu.cn"
+//                );
+//                userRepository.save(AI);
+//            }
+//
+//            String[] topicSet = new String[]{"AI", "Robert"};
+//            // add inner conferences
+//            Conference AIConference = new Conference(userRepository.findByUsername("AI"), "AI abbr", "AI full name", "AI location", LocalDate.of(2020, 4, 12),
+//                    LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), "AI introduction", topicSet);
+//            AIConference.setTopics(new String[]{"AI TOPIC1", "AI TOPIC2"});
+//            AIConference.setStatus(Conference.Status.PASS);
+//            AIConference.setStage(Conference.Stage.CONTRIBUTION);
+//
+//            Conference testRobertConference = new Conference(userRepository.findByUsername("testRobert"), "testRobert abbr", "testRobert full name", "testRobert location", LocalDate.of(2020, 4, 12),
+//                    LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), "testRobert introduction", topicSet);
+//            testRobertConference.setStatus(Conference.Status.PASS);
+//            testRobertConference.setTopics(new String[]{"RO TOPIC1", "RO TOPIC2"});
+//            testRobertConference.setStage(Conference.Stage.CONTRIBUTION);
+//
+//            Conference testRobertConference2 = new Conference(userRepository.findByUsername("testRobert"), "testRobert2 abbr", "testRobert2 full name", "testRobert2 location", LocalDate.of(2020, 4, 12),
+//                    LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), LocalDate.of(2020, 4, 12), "testRobert2 introduction", topicSet);
+//            testRobertConference2.setStatus(Conference.Status.PASS);
+//            testRobertConference2.setTopics(new String[]{"RO TOPIC1", "RO TOPIC2"});
+//            testRobertConference2.setStage(Conference.Stage.CONTRIBUTION);
+//
+//            User tempUser = userRepository.findByUsername("AI");
+//            conferenceRepository.save(AIConference);
+//            userRepository.save(tempUser);
+//
+//            tempUser = userRepository.findByUsername("testRobert");
+//            conferenceRepository.save(testRobertConference);
+//            userRepository.save(tempUser);
+////                userRepository.findByUsername("testRobert").addConference(testRobertConference);
+//
+//
+//            tempUser = userRepository.findByUsername("testRobert");
+//            conferenceRepository.save(testRobertConference2);
+//            userRepository.save(tempUser);
+//
+//            Set<User> userSet = UserGenerator.getRandomUsers(20);
+//            Set<Conference> conferenceSet = ConferenceGenerator.getRandomConferences(20, userSet);
+//            for (User user : userSet
+//            ) {
+//                userRepository.save(user);
+//            }
+//            for (Conference conference : conferenceSet
+//            ) {
+//                conference.setStatus(Conference.Status.PASS);
+//                conferenceRepository.save(conference);
+//            }
+//            // send invitations
+//            ChairIdentityService chairIdentityService = new ChairIdentityService(userRepository,
+//                    invitationRepository, conferenceRepository, paperRepository, reviewRepository, tokenUtil);
+//
+//            ChairSendInvitationRequest chairSendInvitationRequest = new ChairSendInvitationRequest(
+//                    tokenUtil.generateToken(userRepository.findByUsername("AI")),
+//                    AIConference.getConferenceId(),
+//                    new String[]{"robert1", "robert2", "robert3"},
+//                    "message"
+//
+//            );
+//            chairIdentityService.sendInvitation(chairSendInvitationRequest);
+//
+//            // become PC Member
+//            MessageService messageService = new MessageService(userRepository, invitationRepository, conferenceRepository, reviewRepository, tokenUtil);
+//            UserDecideInvitationsRequest userDecideInvitationsRequest1 = new UserDecideInvitationsRequest(
+//                    tokenUtil.generateToken(userRepository.findByUsername("robert1")),
+//                    new ArrayList<>(invitationRepository.findByReviewer(userRepository.findByUsername("robert1"))).get(0).getInvitationId(),
+//                    Invitation.Status.PASS,
+//                    AIConference.getTopics()
+//            );
+//            messageService.userDecideInvitations(userDecideInvitationsRequest1);
+//            UserDecideInvitationsRequest userDecideInvitationsRequest2 = new UserDecideInvitationsRequest(
+//                    tokenUtil.generateToken(userRepository.findByUsername("robert2")),
+//                    new ArrayList<>(invitationRepository.findByReviewer(userRepository.findByUsername("robert2"))).get(0).getInvitationId(),
+//                    Invitation.Status.PASS,
+//                    AIConference.getTopics()
+//            );
+//            messageService.userDecideInvitations(userDecideInvitationsRequest2);
+//            UserDecideInvitationsRequest userDecideInvitationsRequest3 = new UserDecideInvitationsRequest(
+//                    tokenUtil.generateToken(userRepository.findByUsername("robert3")),
+//                    new ArrayList<>(invitationRepository.findByReviewer(userRepository.findByUsername("robert3"))).get(0).getInvitationId(),
+//                    Invitation.Status.PASS,
+//                    AIConference.getTopics()
+//            );
+//            messageService.userDecideInvitations(userDecideInvitationsRequest3);
 
         };
     }
